83bdad4389c927f7e77a98d90fdea2057007f524
Rualin <fateevrual@gmail.com>
2025-03-21 13:39:46 +0000

    Removed using namespace std

--- lab2/task3/task3_1.cpp
+++ lab2/task3/task3_1.cpp
@@ -5,14 +5,12 @@
 #include <cmath>
 #include <fstream>
 #include <vector>
-// #include "functions.cpp"
 
 
-using namespace std;
-typedef vector<double> vd;
+using vec_doub = std::vector<double>;
 
-vector<vd> init_matrix(int m, int n_threads) {
-    vector<vd> res(m, vd(m, 1.0));
+std::vector<vec_doub> init_matrix(int m, int n_threads) {
+    std::vector<vec_doub> res(m, vec_doub(m, 1.0));
     #pragma omp parallel num_threads(n_threads)
     {
         #pragma omp for schedule(dynamic, 100)
@@ -22,43 +20,33 @@ vector<vd> init_matrix(int m, int n_threads) {
     }
     return res;
 }
-vd init_vector(int m, int n_threads) {
-    vd res(m, m + 1);
+vec_doub init_vector(int m, int n_threads) {
+    vec_doub res(m, m + 1);
     return res;
 }
 
-void mul_mat_vec(vector<vd>& mat, vd& vec, vd& res, int m, int n_threads) {
-    // cout << "\tmul_mat_vec\n";
-    // cout << "mat: " << mat.size() << " " << mat.capacity() << "\n";
-    // cout << "mat[0]: " << mat[0].size() << " " << mat[0].capacity() << "\n";
-    // cout << "vec: " << vec.size() << " " << vec.capacity() << "\n";
-    // cout << "res: " << res.size() << " " << res.capacity() << "\n";
+void mul_mat_vec(std::vector<vec_doub>& mat, vec_doub& vec, vec_doub& res, int m, int n_threads) {
     #pragma omp parallel num_threads(n_threads)
     {
         #pragma omp for schedule(dynamic, 100)
         for (int i = 0; i < m; i++) {
-            // cout << "\t\tBefore assigning 0\n";
             res[i] = 0;
             for (int j = 0; j < m; j++) {
-                // cout << "\t\t\tBefore adding\n";
                 res[i] = res[i] + mat[i][j] * vec[j];
             }
         }
     }
-    // return res;
 }
-void sub_vect(vd& a, vd& b, vd& res, int m, int n_threads) {
-    // cout << "\tsub_vect\n";
+void sub_vect(vec_doub& a, vec_doub& b, vec_doub& res, int m, int n_threads) {
     #pragma omp parallel num_threads(n_threads)
     {
         #pragma omp for schedule(dynamic, 100)
         for (int i = 0; i < m; i++) {
             res[i] = a[i] - b[i];
         }
-    }    // return res;
+    }
 }
-void vec_to_scal(vd& a, double val, vd& res, int m, int n_threads) {
-    // cout << "\tvec_to_scal\n";
+void vec_to_scal(vec_doub& a, double val, vec_doub& res, int m, int n_threads) {
     #pragma omp parallel num_threads(n_threads)
     {
         #pragma omp for schedule(dynamic, 100)
@@ -66,11 +54,9 @@ void vec_to_scal(vd& a, double val, vd& res, int m, int n_threads) {
             res[i] = a[i] * val;
         }
     }
-    // return res;
 }
 
-double norm(vd& vec, int m, int n_threads) {
-    // cout << "\tnorm\n";
+double norm(vec_doub& vec, int m, int n_threads) {
     double res = 0.0;
     #pragma omp parallel num_threads(n_threads) 
     {
@@ -82,24 +68,18 @@ double norm(vd& vec, int m, int n_threads) {
     res = sqrt(res);
     return res;
 }
-bool criterion(vector<vd>& A, vd& b, vd& x, double eps, int m, int n_threads) {
-    // cout << "\tcriterion\n";
-    vd sub(m, 0.0);
+bool criterion(std::vector<vec_doub>& A, vec_doub& b, vec_doub& x, double eps, int m, int n_threads) {
+    vec_doub sub(m, 0.0);
     mul_mat_vec(A, x, sub, m, n_threads);
     sub_vect(sub, b, sub, m, n_threads); // must work with two "sub"
     double numerat = norm(sub, m, n_threads);
-    // double numerat = norm(sub_vect(mul_mat_vec(A, x, m, n_threads), b, m, n_threads), m, n_threads);
     double denomin = norm(b, m, n_threads);
-    // free(sub);
     return (numerat / denomin) > eps;
 }
 
-vd easy_iter(vector<vd>& A, vd& b, double t, int m, int n_threads) {
-    // cout << "\teasy_iter\n";
-    // double* x = (double*)calloc(m, sizeof(double));
-    // double* sub = (double*)malloc(m * sizeof(double));
-    vd x(m, 0.0);
-    vd sub(m, 0.0);
+vec_doub easy_iter(std::vector<vec_doub>& A, vec_doub& b, double t, int m, int n_threads) {
+    vec_doub x(m, 0.0);
+    vec_doub sub(m, 0.0);
     int epoch = 0;
     while(criterion(A, b, x, 1.0e-5, m, n_threads)) {
         epoch++;
@@ -108,7 +88,7 @@ vd easy_iter(vector<vd>& A, vd& b, double t, int m, int n_threads) {
         vec_to_scal(sub, t, sub, m, n_threads);
         sub_vect(x, sub, x, m, n_threads);
         if (epoch == 100000) {
-            cout << "The sequence diverges: " << epoch << "\n";
+            std::cout << "The sequence diverges: " << epoch << "\n";
             exit(1);
         }
     }
@@ -116,41 +96,18 @@ vd easy_iter(vector<vd>& A, vd& b, double t, int m, int n_threads) {
 }
 
 int main(int argc, char** argv) {
-    cout << "Start\n";
     int m = atol(argv[1]);
     // double t = atof(argv[2]);
     int n_threads = atol(argv[2]);
-    cout << "Before inits\n";
-    vector<vd> A = init_matrix(m, n_threads);
-    vd b = init_vector(m, n_threads);
+    std::vector<vec_doub> A = init_matrix(m, n_threads);
+    vec_doub b = init_vector(m, n_threads);
     double t = 1.0 / m;
-    // cout << t << "\n";
-    // cout << "Max threds: " << omp_get_max_threads() << "\n";
-    cout << "Before timer\n";
-    const auto start{chrono::steady_clock::now()};
-    vd res = easy_iter(A, b, t, m, n_threads);
-    const auto end{chrono::steady_clock::now()};
-    cout << "After timer\n";
-    const chrono::duration<double> elapsed_seconds{end - start};
-    cout << "Before file\n";
-    fstream fout("result.csv", fstream::out | fstream::app);
+    const auto start{std::chrono::steady_clock::now()};
+    vec_doub res = easy_iter(A, b, t, m, n_threads);
+    const auto end{std::chrono::steady_clock::now()};
+    const std::chrono::duration<double> elapsed_seconds{end - start};
+    std::fstream fout("result.csv", std::fstream::out | std::fstream::app);
     fout << n_threads << "," << elapsed_seconds.count() << "\n";
-    // cout << "Result:\n";
-    // cout << res[0] << " " << res[1] << " " << res[2] << " ... ";
-    // cout << res[m - 3] << " " << res[m - 2] << " " << res[m - 1] << "\n";
-    // cout << "Duration: " << elapsed_seconds.count() << "\n";
-    // #pragma omp parallel num_threads(n_threads)
-    // {
-    //     #pragma omp for schedule(dynamic, 100)
-    //     for (int i = 0; i < m; i++) {
-    //         free(A[i]);
-    //     }
-    // }
-    // free(A);
-    // free(b);
-    // free(res);
     fout.close();
-    cout << "End\n";
-    // cout << "Max threds: " << omp_get_max_threads() << "\n";
     return 0;
 }
--- lab2/task3/task3_2.cpp
+++ lab2/task3/task3_2.cpp
@@ -6,100 +6,77 @@
 #include <fstream>
 #include <vector>
 
+#define type dynamic
+#define K 500
+using vec_doub = std::vector<double>;
 
-using namespace std;
-typedef vector<double> vd;
-
-vector<vd> init_matrix(int m, int n_threads) {
-    vector<vd> res(m, vd(m, 1.0));
+std::vector<vec_doub> init_matrix(int m, int n_threads) {
+    std::vector<vec_doub> res(m, vec_doub(m, 1.0));
     #pragma omp parallel num_threads(n_threads)
     {
-        #pragma omp for schedule(dynamic, 100)
+        #pragma omp for schedule(type, K)
         for (int i = 0; i < m; i++) {
             res[i][i] = 2.0;
         }
     }
     return res;
 }
-vd init_vector(int m, int n_threads) {
-    vd res(m, m + 1);
+vec_doub init_vector(int m, int n_threads) {
+    vec_doub res(m, m + 1);
     return res;
 }
 
-void inline mul_mat_vec(vector<vd>& mat, vd& vec, vd& res, int m) {
-    #pragma omp for schedule(dynamic, 100)
+void inline mul_mat_vec(std::vector<vec_doub>& mat, vec_doub& vec, vec_doub& res, int m) {
+    #pragma omp for schedule(type, K)
     for (int i = 0; i < m; i++) {
         res[i] = 0;
         for (int j = 0; j < m; j++) {
             res[i] += mat[i][j] * vec[j];
         }
     }
-    // return res;
 }
-void inline sub_vect(vd& a, vd& b, vd& res, int m) {
-    #pragma omp for schedule(dynamic, 100)
+void inline sub_vect(vec_doub& a, vec_doub& b, vec_doub& res, int m) {
+    #pragma omp for schedule(type, K)
     for (int i = 0; i < m; i++) {
         res[i] = a[i] - b[i];
     }
-    // return res;
 }
-void inline vec_to_scal(vd& a, double val, vd& res, int m) {
-    #pragma omp for schedule(dynamic, 100)
+void inline vec_to_scal(vec_doub& a, double val, vec_doub& res, int m) {
+    #pragma omp for schedule(type, K)
     for (int i = 0; i < m; i++) {
         res[i] = a[i] * val;
     }
-    // return res;
 }
 
-double norm(vd& vec, int m) {
+double norm(vec_doub& vec, int m) {
     double res = 0.0;
-    // #pragma omp for schedule(dynamic, 100) reduction(+:res)
     for(int i = 0; i < m; i++) {
         res += vec[i] * vec[i];
     }
     res = sqrt(res);
     return res;
 }
-bool criterion(vector<vd>& A, vd& b, vd& x, double eps, int m) {
-    // double* sub = (double*)calloc(m, sizeof(double));
-    vd sub(m, 0.0);
+bool criterion(std::vector<vec_doub>& A, vec_doub& b, vec_doub& x, double eps, int m) {
+    vec_doub sub(m, 0.0);
     mul_mat_vec(A, x, sub, m);
     sub_vect(sub, b, sub, m); // must work with two "sub"
     double numerat = norm(sub, m);
-    // double numerat = norm(sub_vect(mul_mat_vec(A, x, m, n_threads), b, m, n_threads), m, n_threads);
     double denomin = norm(b, m);
-    // free(sub);
     return (numerat / denomin) > eps;
 }
 
-vd easy_iter(vector<vd>& A, vd& b, double t, int m, int n_threads) {
-    // double* x = (double*)calloc(m, sizeof(double));
-    // double* sub = (double*)malloc(m * sizeof(double));
-    vd x(m, 0.0);
-    vd sub(m, 0.0);
+vec_doub easy_iter(std::vector<vec_doub>& A, vec_doub& b, double t, int m, int n_threads) {
+    vec_doub x(m, 0.0);
+    vec_doub sub(m, 0.0);
     bool criter = true;
     double numerat, denomin;
     while(criter) {
         #pragma omp parallel num_threads(n_threads) shared(x, sub, criter, numerat, denomin)
         {
             mul_mat_vec(A, x, sub, m);
-
             sub_vect(sub, b, sub, m); // must work with two "sub"
             vec_to_scal(sub, t, sub, m);
             sub_vect(x, sub, x, m);
-            // #pragma omp barrier
-            // #pragma omp single
-            // {
-            // cout << x[0] << " " << x[1] << " " << x[2] << " ... ";
-            // cout << x[m - 3] << " " << x[m - 2] << " " << x[m - 1] << "\n";
-            // }
-            // if (epoch == 10000) {
-            //     cout << "The sequence diverges: " << epoch << "\n";
-            //     exit(1);
-            // }
-            // criter = criterion(A, b, x, 1.0e-5, m);
-            
-            //criterion calc start
             mul_mat_vec(A, x, sub, m);
             sub_vect(sub, b, sub, m); // must work with two "sub"
             #pragma omp single
@@ -108,39 +85,23 @@ vd easy_iter(vector<vd>& A, vd& b, double t, int m, int n_threads) {
             denomin = norm(b, m);
             #pragma omp barrier
             criter = (numerat / denomin) > 1.0e-5;
-            //criterion calc end
         }
     }
-    // free(sub);
     return x;
 }
 
 int main(int argc, char** argv) {
     int m = atol(argv[1]);
     int n_threads = atol(argv[2]);
-    vector<vd> A = init_matrix(m, n_threads);
-    vd b = init_vector(m, n_threads);
+    std::vector<vec_doub> A = init_matrix(m, n_threads);
+    vec_doub b = init_vector(m, n_threads);
     double t = 1.0 / m;
-    const auto start{chrono::steady_clock::now()};
-    vd res = easy_iter(A, b, t, m, n_threads);
-    const auto end{chrono::steady_clock::now()};
-    const chrono::duration<double> elapsed_seconds{end - start};
-    // cout << "Result:\n";
-    // cout << res[0] << " " << res[1] << " " << res[2] << " ... ";
-    // cout << res[m - 3] << " " << res[m - 2] << " " << res[m - 1] << "\n";
-    // cout << elapsed_seconds.count() << "\n";
-    fstream fout("result2.csv", fstream::out | fstream::app);
-    fout << n_threads << "," << elapsed_seconds.count() << "\n";
-    // #pragma omp parallel num_threads(n_threads)
-    // {
-    //     #pragma omp for schedule(dynamic, 100)
-    //     for (int i = 0; i < m; i++) {
-    //         free(A[i]);
-    //     }
-    // }
-    // free(A);
-    // free(b);
-    // free(res);
+    const auto start{std::chrono::steady_clock::now()};
+    vec_doub res = easy_iter(A, b, t, m, n_threads);
+    const auto end{std::chrono::steady_clock::now()};
+    const std::chrono::duration<double> elapsed_seconds{end - start};
+    std::fstream fout("res_dynamic.csv", std::fstream::out | std::fstream::app);
+    fout << n_threads << "," << elapsed_seconds.count() << "," << K << "\n";
     fout.close();
     return 0;
 }
